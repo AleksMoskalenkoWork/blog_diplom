831056fdb59403d2f56f2968788262f14a9eefe4
AleksMoskalenkoWork <aleks.moskalenko.work@gmail.com>
2025-08-05 12:13:02 +0300

    fix empty comment field

--- routes/comments.js
+++ routes/comments.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const he = require('he');
 const Comment = require('../schema/comment');
 const Article = require('../schema/article');
+const { text } = require('body-parser');
 
 module.exports = function () {
   router.post('/:url/comments', async (req, res) => {
@@ -10,6 +11,17 @@ module.exports = function () {
       const url = req.params.url;
       const author = req.session.userId;
       const article = await Article.findOne({ url });
+      const comments = await Comment.find({ article: article._id }).populate(
+        'author'
+      );
+
+      if (!req.body.comment || !req.body.comment.trim()) {
+        return res.render('article', {
+          article,
+          comments,
+          error: 'Comment field cannot be empty.',
+        });
+      }
 
       await Comment.insertOne({
         author,
